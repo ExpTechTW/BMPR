7b5ecba83de2cd3638d11c011b59985ad8ec01db
whes1015 <44525760+whes1015@users.noreply.github.com>
2022-06-11 23:58:25 +0800

    Delete B-MPR-Release directory

--- B-MPR-Release/core/handler.js
+++
@@ -1,9 +0,0 @@
-const reload = require('require-reload')(require)
-
-async function main(msg) {
-    if (msg.startsWith("mpr upgrade")) reload('./update').Update_core()
-}
-
-module.exports = {
-    main
-}
