fecd8ab613c67e98c7f943b1afe5f024599312bd
whes1015 <44525760+whes1015@users.noreply.github.com>
2022-06-11 23:20:21 +0800

    Add files via upload

---
+++ BMPR.js
@@ -0,0 +1,34 @@
+const reload = require('require-reload')(require)
+const Console = reload('./B-MPR-Release/core/console')
+const Structure = reload('./B-MPR-Release/core/structure')
+const Loader = reload('./B-MPR-Release/core/loader')
+const Config = reload('./B-MPR-Release/core/api/config')
+const Handler = reload('./B-MPR-Release/core/handler')
+const Time = reload('./B-MPR-Release/core/api/time')
+const User = reload('./B-MPR-Release/core/api/user')
+const Help = reload('./B-MPR-Release/core/api/help')
+const Rely = reload('./B-MPR-Release/core/api/rely')
+const Permission = reload('./B-MPR-Release/core/api/permission')
+
+let Info = {
+    "version": "1.0.0"
+}
+
+async function main(bmpr) {
+    reload('./B-MPR-Release/core/client').main(bmpr)
+}
+
+module.exports = {
+    Info,
+    main,
+    Console,
+    Structure,
+    Loader,
+    Config,
+    Handler,
+    Time,
+    User,
+    Permission,
+    Help,
+    Rely
+}
