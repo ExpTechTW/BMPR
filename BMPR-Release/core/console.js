dab7ae6e0046f9fa25166506cc0f2f03795ef20e
whes1015 <44525760+whes1015@users.noreply.github.com>
2022-06-12 23:48:59 +0800

    Add files via upload

--- BMPR-Release/core/console.js
+++ BMPR-Release/core/console.js
@@ -26,89 +26,93 @@ async function init(client, config, bmpr) {
 }
 
 async function main(msg, level, sender, fun, client) {
-    if (client != undefined) {
-        c = await client.channels.fetch(Config["Bot.Console"]).catch(err => {
-            main(`日誌輸出到 Discord 時發生異常 >> ${error}`, 4, "Core", "Console")
-        })
-    }
-    if (sender == undefined) sender = "N/A"
-    if (fun == undefined) fun = "Main"
-    if (level == undefined) level = 2
-    // Track 0
-    // Debug 1
-    // Info 2
-    // Warn 3
-    // Error 4
-    let Type = ""
-    if (level == 0) Type = "Track"
-    if (level == 1) Type = "Debug"
-    if (level == 2) Type = "Info"
-    if (level == 3) Type = "Warn"
-    if (level == 4) Type = "Error"
-    if (level == 5) Type = "Console"
-    if (Config["Bot.Console.outLevel"] <= level && level != 5) {
-        if (level == 1) {
-            console.log(`[${sender}][${await Time.Simple()}][${fun}]: ${msg}`)
-        } else if (level == 0) {
-            console.log("\x1b[90m" + `[${sender}][${await Time.Simple()}][${fun}]: ${msg}` + "\x1b[0m")
-        } else if (level == 4) {
-            console.log("\x1b[31m" + `[${sender}][${await Time.Simple()}][${fun}]: ${msg}` + "\x1b[0m")
-        } else if (level == 3) {
-            console.log("\x1b[33m" + `[${sender}][${await Time.Simple()}][${fun}]: ${msg}` + "\x1b[0m")
-        } else {
-            console.log("\x1b[32m" + `[${sender}][${await Time.Simple()}][${fun}]: ${msg}` + "\x1b[0m")
+    try {
+        if (client != undefined) {
+            c = await client.channels.fetch(Config["Bot.Console"]).catch(err => {
+                main(`日誌輸出到 Discord 時發生異常 >> ${err}`, 4, "Core", "Console")
+            })
         }
-        if (c == null) return
-        if (cache == level) {
-            let MSG = await c.messages.fetch(msgID.id)
-            body += `\n[${sender}][${await Time.Simple()}][${fun}]: ${msg}`
+        if (sender == undefined) sender = "N/A"
+        if (fun == undefined) fun = "Main"
+        if (level == undefined) level = 2
+        // Track 0
+        // Debug 1
+        // Info 2
+        // Warn 3
+        // Error 4
+        let Type = ""
+        if (level == 0) Type = "Track"
+        if (level == 1) Type = "Debug"
+        if (level == 2) Type = "Info"
+        if (level == 3) Type = "Warn"
+        if (level == 4) Type = "Error"
+        if (level == 5) Type = "Console"
+        let now = new Date()
+        if (!fs.existsSync(path.resolve(`./Database/log/`))) {
+            fs.mkdirSync(`./Database/log/`)
+        }
+        if (!fs.existsSync(path.resolve(`./Database/log/${(now.getMonth() + 1)}`))) {
+            fs.mkdirSync(path.resolve(`./Database/log/${(now.getMonth() + 1)}`))
+        }
+        if (!fs.existsSync(path.resolve(`./Database/log/${(now.getMonth() + 1)}/${now.getDate()}`))) {
+            fs.mkdirSync(path.resolve(`./Database/log/${(now.getMonth() + 1)}/${now.getDate()}`))
+        }
+        if (!fs.existsSync(path.resolve(`./Database/log/${(now.getMonth() + 1)}/${now.getDate()}`))) {
+            fs.mkdirSync(path.resolve(`./Database/log/${(now.getMonth() + 1)}/${now.getDate()}`))
+        }
+        if (!fs.existsSync(path.resolve(`./Database/log/${(now.getMonth() + 1)}/${now.getDate()}/${now.getHours()}.log`))) {
+            fs.writeFileSync(path.resolve(`./Database/log/${(now.getMonth() + 1)}/${now.getDate()}/${now.getHours()}.log`), "", 'utf8')
+        }
+        let Log = fs.readFileSync(path.resolve(`./Database/log/${(now.getMonth() + 1)}/${now.getDate()}/${now.getHours()}.log`))
+        Log = `[${sender}][${await Time.Simple()}][${fun}/${Type}]: ${msg}\n` + Log
+        fs.writeFileSync(path.resolve(`./Database/log/${(now.getMonth() + 1)}/${now.getDate()}/${now.getHours()}.log`), Log, 'utf8')
+        if (Config["Bot.Console.outLevel"] <= level && level != 5) {
             if (level == 1) {
-                await MSG.edit(await Structure.embed("**日誌**\n\n```" + body + "```", "#FFFFFF"))
+                console.log(`[${sender}][${await Time.Simple()}][${fun}]: ${msg}`)
             } else if (level == 0) {
-                await MSG.edit(await Structure.embed("**追蹤**\n\n```" + body + "```", "#6C6C6C"))
+                console.log("\x1b[90m" + `[${sender}][${await Time.Simple()}][${fun}]: ${msg}` + "\x1b[0m")
             } else if (level == 4) {
-                await MSG.edit(await Structure.embed("**錯誤**\n\n```" + body + "```", "#FF0000"))
+                console.log("\x1b[31m" + `[${sender}][${await Time.Simple()}][${fun}]: ${msg}` + "\x1b[0m")
             } else if (level == 3) {
-                await MSG.edit(await Structure.embed("**警告**\n\n```" + body + "```", "#FF9224"))
+                console.log("\x1b[33m" + `[${sender}][${await Time.Simple()}][${fun}]: ${msg}` + "\x1b[0m")
             } else {
-                await MSG.edit(await Structure.embed("**訊息**\n\n```" + body + "```", "#00EC00"))
+                console.log("\x1b[32m" + `[${sender}][${await Time.Simple()}][${fun}]: ${msg}` + "\x1b[0m")
             }
-            check()
-        } else {
-            cache = level
-            body = `[${sender}][${await Time.Simple()}][${fun}]: ${msg}`
-            if (level == 1) {
-                msgID = await c.send(await Structure.embed("**日誌**\n\n```" + body + "```", "#FFFFFF"))
-            } else if (level == 0) {
-                msgID = await c.send(await Structure.embed("**追蹤**\n\n```" + body + "```", "#6C6C6C"))
-            } else if (level == 4) {
-                msgID = await c.send(await Structure.embed("**錯誤**\n\n```" + body + "```", "#FF0000"))
-            } else if (level == 3) {
-                msgID = await c.send(await Structure.embed("**警告**\n\n```" + body + "```", "#FF9224"))
+            if (c == null) return
+            if (cache == level) {
+                let MSG = await c.messages.fetch(msgID.id)
+                body += `\n[${sender}][${await Time.Simple()}][${fun}]: ${msg}`
+                if (level == 1) {
+                    await MSG.edit(await Structure.embed("**日誌**\n\n```" + body + "```", "#FFFFFF"))
+                } else if (level == 0) {
+                    await MSG.edit(await Structure.embed("**追蹤**\n\n```" + body + "```", "#6C6C6C"))
+                } else if (level == 4) {
+                    await MSG.edit(await Structure.embed("**錯誤**\n\n```" + body + "```", "#FF0000"))
+                } else if (level == 3) {
+                    await MSG.edit(await Structure.embed("**警告**\n\n```" + body + "```", "#FF9224"))
+                } else {
+                    await MSG.edit(await Structure.embed("**訊息**\n\n```" + body + "```", "#00EC00"))
+                }
+                check()
             } else {
-                msgID = await c.send(await Structure.embed("**訊息**\n\n```" + body + "```", "#00EC00"))
+                cache = level
+                body = `[${sender}][${await Time.Simple()}][${fun}]: ${msg}`
+                if (level == 1) {
+                    msgID = await c.send(await Structure.embed("**日誌**\n\n```" + body + "```", "#FFFFFF"))
+                } else if (level == 0) {
+                    msgID = await c.send(await Structure.embed("**追蹤**\n\n```" + body + "```", "#6C6C6C"))
+                } else if (level == 4) {
+                    msgID = await c.send(await Structure.embed("**錯誤**\n\n```" + body + "```", "#FF0000"))
+                } else if (level == 3) {
+                    msgID = await c.send(await Structure.embed("**警告**\n\n```" + body + "```", "#FF9224"))
+                } else {
+                    msgID = await c.send(await Structure.embed("**訊息**\n\n```" + body + "```", "#00EC00"))
+                }
             }
         }
+    } catch (err) {
+        main(`日誌輸出到 Discord 時發生異常 >> ${err}`, 4, "Core", "Console")
     }
-    let now = new Date()
-    if (!fs.existsSync(path.resolve(`./Database/log/`))) {
-        fs.mkdirSync(`./Database/log/`)
-    }
-    if (!fs.existsSync(path.resolve(`./Database/log/${(now.getMonth() + 1)}`))) {
-        fs.mkdirSync(path.resolve(`./Database/log/${(now.getMonth() + 1)}`))
-    }
-    if (!fs.existsSync(path.resolve(`./Database/log/${(now.getMonth() + 1)}/${now.getDate()}`))) {
-        fs.mkdirSync(path.resolve(`./Database/log/${(now.getMonth() + 1)}/${now.getDate()}`))
-    }
-    if (!fs.existsSync(path.resolve(`./Database/log/${(now.getMonth() + 1)}/${now.getDate()}`))) {
-        fs.mkdirSync(path.resolve(`./Database/log/${(now.getMonth() + 1)}/${now.getDate()}`))
-    }
-    if (!fs.existsSync(path.resolve(`./Database/log/${(now.getMonth() + 1)}/${now.getDate()}/${now.getHours()}.log`))) {
-        fs.writeFileSync(path.resolve(`./Database/log/${(now.getMonth() + 1)}/${now.getDate()}/${now.getHours()}.log`), "", 'utf8')
-    }
-    let Log = fs.readFileSync(path.resolve(`./Database/log/${(now.getMonth() + 1)}/${now.getDate()}/${now.getHours()}.log`))
-    Log = `[${sender}][${await Time.Simple()}][${fun}/${Type}]: ${msg}\n` + Log
-    fs.writeFileSync(path.resolve(`./Database/log/${(now.getMonth() + 1)}/${now.getDate()}/${now.getHours()}.log`), Log, 'utf8')
 }
 
 process.stdin.on('data', async data => {
