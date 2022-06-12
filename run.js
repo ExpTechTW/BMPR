a7d14ba42dece194b0da46f0759ee4f5a0b65584
whes1015 <44525760+whes1015@users.noreply.github.com>
2022-06-12 23:29:32 +0800

    Add files via upload

--- run.js
+++ run.js
@@ -25,22 +25,25 @@ async function Update() {
     setInterval(async () => {
         let list = fs.readdirSync(path.resolve("./Database/cache"))
         if (list.includes("update.tmp")) {
+            if (fs.existsSync(path.resolve("./BMPR-Release.zip"))) fs.unlinkSync(path.resolve("./BMPR-Release.zip"))
             let res = await fetch("https://raw.githubusercontent.com/ExpTechTW/BMPR/Release/BMPR.js")
             let text = await res.text()
             fs.writeFileSync(path.resolve("./BMPR.js"), text)
-            const downloader = new Downloader({
-                url: "https://github.com/ExpTechTW/BMPR/archive/refs/heads/Release.zip",
-                directory: "./",
-            })
             try {
+                const downloader = new Downloader({
+                    url: "https://github.com/ExpTechTW/BMPR/archive/refs/heads/Release.zip",
+                    directory: "./",
+                })
                 await downloader.download()
                 const unzip = new zl.Unzip()
-                await unzip.extract("./BMPR-Release.zip", path.resolve(""))
-                fs.unlinkSync("./BMPR-Release.zip")
+                await unzip.extract(path.resolve("./BMPR-Release.zip"), path.resolve(""))
             } catch (error) {
+                fs.unlinkSync(path.resolve("./BMPR-Release.zip"))
                 console.log("Download failed", error)
             }
+            fs.unlinkSync(path.resolve("./BMPR-Release.zip"))
             fs.writeFileSync(path.resolve("./Database/cache/reload.tmp"), "")
+            process.exit(0)
         }
     }, 1000)
 }
